4b45781869d4a60e1cbebadb065061147e665ba2
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 23:33:30 +0545

    Add files via upload

---
+++ dowhileloop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class dowhileloop
+    {
+        static void Main(string[] args)
+        {
+            //execute using do while loop
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Value of i: {0}", i);
+
+                i++;
+
+            } while (i < 10);
+
+            Console.ReadLine();
+
+        }
+    }
+}
