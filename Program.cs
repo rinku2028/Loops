02b2e989e89ae4243f6225c9bb70a40f7bd1efc8
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 22:48:52 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i=1;i<=20;i++)
+            {
+                Console.WriteLine("Value of i:{0}", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
