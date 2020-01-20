d5690f56a232368c555f616549d8393211de044e
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 23:25:28 +0545

    Add files via upload

---
+++ breakwhileloop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class breakwhileloop
+    {
+        static void Main(string[] args)
+        {
+            //break in while loop
+            int i = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Value of i: {0}", i);
+
+                i++;
+
+                if (i > 10)
+                    break;
+            }
+        }
+    }
+}
