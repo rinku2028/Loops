c47ebab1966ce4ec4fcb85e4f9826cc539f214b7
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 23:37:36 +0545

    Add files via upload

---
+++ switchin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class switchin
+    {
+        static void Main(string[] args)
+        {
+            int i = 15;
+
+            switch (i)
+            {
+                case 5:
+                    Console.WriteLine("The value of i is 5");
+                    break;
+                case 10:
+                    Console.WriteLine("The value of i is 10");
+                    break;
+                case 15:
+                    Console.WriteLine("The value of i is 15");
+                    break;
+                default:
+                    Console.WriteLine("Unknown value");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
