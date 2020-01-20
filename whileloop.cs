137ddfb0ab04b021eaa9e271c8e3db29cfaa939f
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 23:22:28 +0545

    Add files via upload

---
+++ whileloop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class whileloop
+    {
+        static void Main(string[] args)
+        {
+            //using while loop to execute block of code repeatedly
+            int y = 0;
+
+            while (y < 10)
+            {
+                Console.WriteLine("The value  of y: {0}", y);
+
+                y++;
+            }
+        }
+    }
+}
