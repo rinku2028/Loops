c5c8d39ac9b062767d38146412303db1f957d0c7
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 23:30:12 +0545

    Add files via upload

---
+++ nestedwhileloop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class nestedwhileloop
+    {
+
+        static void Main(string[] args)
+        {
+            //nested while loop in c#
+            int i = 0;
+
+            while (i < 2)
+            {
+                Console.WriteLine("Value of i: {0}", i);
+                int j = 1;
+
+                i++;
+
+                while (j < 2)
+                {
+                    Console.WriteLine("Print value of j: {0}", j);
+                    j++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
