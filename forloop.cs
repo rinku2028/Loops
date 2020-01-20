b8f4ae344351e3f89a7a44ecdddcf34b40804cf7
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 23:01:44 +0545

    Add files via upload

---
+++ forloop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class forloop
+    {
+        static void Main(string[] args)
+        {
+            //Printing values from 0 to 9 in loop
+            int x;
+            for (x = 0; x<10; x++)
+            {
+                Console.WriteLine("Value of x:{0}", x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
