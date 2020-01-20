caf9be112b41faf1b8794282414b9f76832fe6ad
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 23:11:27 +0545

    Add files via upload

---
+++ doubleforloop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class doubleforloop
+    {
+        static void Main(string[] args)
+        {
+            for (double d = 1.01D; d < 1.10; d += 0.01D)
+            {
+                Console.WriteLine("Value of i: {0}", d);
+            }
+            Console.ReadLine();
+        }
+    }
+}
