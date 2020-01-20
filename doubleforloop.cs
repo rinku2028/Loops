ad15e952f9297f2b740eb527c48764ee874f5c5e
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-20 23:12:07 +0545

    Update doubleforloop.cs

--- doubleforloop.cs
+++ doubleforloop.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp5
 {
     class doubleforloop
     {
+        //printing decimal values in for loop
         static void Main(string[] args)
         {
             for (double d = 1.01D; d < 1.10; d += 0.01D)
