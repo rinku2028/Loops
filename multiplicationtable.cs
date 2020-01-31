4836035f078d3cb5eee89f8a9834b571af191a65
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-31 23:54:51 +0545

    Add files via upload

---
+++ multiplicationtable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace multiplicationtable
+{
+    class multiplicationtable
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<10;i++)
+            {
+                int j = i * 5;
+                Console.WriteLine(j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
