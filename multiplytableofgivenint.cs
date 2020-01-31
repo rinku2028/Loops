15c9d50a53a4e7d97269fecf3426c29f4fe7ccaa
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-31 23:55:49 +0545

    Multiplication Table

---
+++ multiplytableofgivenint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace multiplicationtable
+{
+    class multiplytableofgivenint
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            Console.WriteLine("DISPLAY MULTIPLICAITON TALBE:\n");
+            Console.WriteLine("Please input number to be calculated:");
+            j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n"); //New line
+
+            for(i=1;i<=10;i++)
+            {
+                Console.WriteLine("{0}x{1}={2}\n",j,i,i * j);
+            }
+            Console.ReadLine();
+        }
+    }
+}
