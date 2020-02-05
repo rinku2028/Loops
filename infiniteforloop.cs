119bf157a66223c17f4f461696034effebd0514c
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-05 18:59:43 +0545

    Add files via upload

---
+++ infiniteforloop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class infiniteloop
+    {
+        static void Main(string []args)
+        {
+            for(; ;)
+            {
+                Console.WriteLine("Hello! This is infinite loop!");
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
