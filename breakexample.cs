c24bd2fe68f4fff39a3e4d3d202fd2d21a974ca5
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-09 23:16:45 +0545

    Add files via upload

---
+++ breakexample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    class breakexample
+    {
+        static void Main(string []args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine("The number is " + i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ continueexample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class continueexample
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine("The number is"+ i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
