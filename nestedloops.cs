356e863b0e38e605224071852dd68a89f9a1f815
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-11 23:51:09 +0545

    Nested Loops

---
+++ nestedloops.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    class nestedloops
+    {
+        static void Main(string []args)
+        {
+            // loop within loop printing Nested Loops
+            for (int i = 2; i < 3; i++)
+                for (int j = 1; j < i; j++)
+                    Console.WriteLine("NestedLoops");
+            Console.ReadLine();
+        }
+    }
+}
